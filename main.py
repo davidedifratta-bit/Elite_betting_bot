799b9212519d0154c42807c97ba7d712a03070cc
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-05-14 17:47:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
+import random
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update: Update, context):
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot online!")
 
-async def signal(update: Update, context):
+
+async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    matches = [
+        "⚽ Inter vs Milan",
+        "⚽ Arsenal vs Chelsea",
+        "⚽ Real Madrid vs Barcelona"
+    ]
+
+    match = random.choice(matches)
+
     await update.message.reply_text(
-        "🔥 DAILY SIGNAL\n\n"
-        "⚽ Milan vs Roma\n"
-        "✅ Over 2.5 Goals @1.85\n"
-        "Confidence: 82%"
+        f"🔥 DAILY SIGNAL\n\n"
+        f"{match}\n"
+        f"✅ Over 2.5 Goals @1.85\n"
+        f"Confidence: 82%"
     )
+
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("signal", signal))
+
 print("Bot avviato...")
 
 app.run_polling()
-
