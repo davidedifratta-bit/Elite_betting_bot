65dd05e7473b13a70ed9cb6c3e8b806bef5c2729
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-05-14 18:20:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    ContextTypes
-)
-
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import random
 import os
 
