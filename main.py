559b587287e7c599426aee235fe3e4f095e20e54
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-05-15 00:29:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,20 +43,26 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     match = random.choice(matches)
-
-    await update.message.reply_text(
-        f"🔥 DAILY SIGNAL\n\n"
-        f"{match}\n"
-        f"✅ Over 2.5 Goals @1.85\n"
-        f"Confidence: 82%"
-    )
+await update.message.reply_text(
+    f"🔥 ELITE DAILY PICK 🔥\n\n"
+f"⚽ Match: {match}\n"
+f"🎯 Market: Over 2.5 Goals\n"
+f"📈 Odds: 1.85\n"
+f"💰 Stake: 8/10\n"
+f"📊 Confidence: 82%\n\n"
+f"💎 Elite Betting Lab"
+ )   
     await context.bot.send_message(
     chat_id=CHANNEL_ID,
     text=
-        f"🔥 DAILY SIGNAL\n\n"
-        f"{match}\n"
-        f"✅ Over 2.5 Goals @1.85\n"
-        f"Confidence: 82%"
+        
+f"🔥 ELITE DAILY PICK 🔥\n\n"
+f"⚽ Match: {match}\n"
+f"🎯 Market: Over 2.5 Goals\n"
+f"📈 Odds: 1.85\n"
+f"💰 Stake: 8/10\n"
+f"📊 Confidence: 82%\n\n"
+f"💎 Elite Betting Lab"        
 )
 
 async def over(update: Update, context: ContextTypes.DEFAULT_TYPE):
