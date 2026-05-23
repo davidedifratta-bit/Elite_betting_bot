8d8669f5f7227fc5fb98f4ea7f57cf10ddb19caf
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-05-23 17:40:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ FOOTYSTATS_API_KEY = os.getenv("FOOTYSTATS_API_KEY")
 used_matches = []
 def get_footystats_prediction():
 
-    url = f"https://api.footystats.org/todays-matches?key={FOOTYSTATS_API_KEY}"
+    url = f"https://api.footystats.org/matches?key={FOOTYSTATS_API_KEY}"
 
     response = requests.get(url)
     
