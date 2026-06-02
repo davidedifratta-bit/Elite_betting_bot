e67c1a8fa58a25b2316580bffbf0ac8c77346616
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-06-02 16:59:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,13 @@ if data["success"] and len(data["data"]) > 0:
 
         if int(m.get("btts_potential", 0)) < 50:
             continue
+
+        if float(m.get("team_a_xg_prematch", 0)) < 1.2:
+            continue
+
+        if float(m.get("team_b_xg_prematch", 0)) < 1.0:
+            continue
+            
         score = (
             int(m.get("o25_potential", 0))
             + int(m.get("btts_potential", 0))
