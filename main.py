d061297b64b098761aac7bc74bc9d816aab23d97
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-06-04 17:21:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,8 @@ if data["success"] and len(data["data"]) > 0:
     best_score = 0
 
     for m in data["data"]:
-        print(m)
+        print("KEYS:", m.keys())
+        break
         if int(m.get("o25_potential", 0)) < 60:
             continue
 
