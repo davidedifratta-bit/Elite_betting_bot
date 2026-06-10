0cf1bf09d41b0e93d532501a1fc625c9d3544002
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-06-10 21:11:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -151,6 +151,7 @@ if data["success"] and len(data["data"]) > 0:
             + int(float(m.get("team_b_xg_prematch", 0)) * 10)
             + int(float(m.get("corners_potential", 0)))
 )
+        print("SCORE CALCOLATO:", score)
         if score < 220:
             continue
     
