bd86ef8e57d8aca8535550c75f4f1cdb6366a08d
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-06-10 21:34:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -154,6 +154,7 @@ if data["success"] and len(data["data"]) > 0:
         print("SCORE CALCOLATO:", score)
         print("PITBULL TEST 999")
         if score < 220:
+            print("SCARTATA SCORE:", score)
             continue
     
         print(
