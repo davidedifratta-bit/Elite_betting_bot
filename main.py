9f39bd23c98427fd0a3743fa32a85441086078f6
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-06-15 02:35:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -235,6 +235,7 @@ if data["success"] and len(data["data"]) > 0:
     print("PARTITA ACCETTATA:", over25_odds)
     best_score = score
     best_match = m
+    print("DEBUG BEST SCORE=", best_score)
     print("NUOVO BEST SALVATO:")
     print(best_match.get("home_name"), "vs", best_match.get("away_name"))
     print("BEST SCORE:", best_score)
