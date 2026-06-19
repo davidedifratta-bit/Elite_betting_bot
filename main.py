6142bef939a3d00b0cd57b29025a36b3ebfaee4d
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-06-19 03:36:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ if data["success"] and len(data["data"]) > 0:
             print("MATCH:", m.get("home_name"), "vs", m.get("away_name"))
             continue
 
-        if over25_odds > 2.70:
+        if over25_odds > 1.70:
             print("STOP QUOTA ALTA")
             print("MATCH:", m.get("home_name"), "vs", m.get("away_name"))
             continue
