350bd3007b5f7d1abc35178a65d3ccd92a74614a
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-08-26 01:26:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -222,10 +222,10 @@ if data["success"] and len(data["data"]) > 0:
         score = (
             int(m.get("o25_potential", 0))
             + int(m.get("btts_potential", 0))
-            + int(float(m.get("team_a_xg_prematch", 0)) * 10)
-            + int(float(m.get("team_b_xg_prematch", 0)) * 10)
-            + int(float(m.get("corners_potential", 0)))
-)
+            + int(float(m.get("team_a_xg_prematch", 0)) * 25)
+            + int(float(m.get("team_b_xg_prematch", 0)) * 25)
+            + int(m.get("corners_potential", 0))
+        )
         print(
             "SCORE CALCOLATO:",
             m.get("home_name"),
